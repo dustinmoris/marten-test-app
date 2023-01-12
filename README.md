000fe8166eb898283dd645bf9f049486c7a71f1a
Dustin Moris Gorski <dustinmoris@gmail.com>
2023-01-12 12:44:46 +0000

    added demo app

---
+++ README.md
@@ -0,0 +1,15 @@
+README
+======
+
+Start demo app:
+
+```
+docker compose up
+```
+
+Connect to PostgreSQL:
+
+```
+docker exec -it postgres bash
+psql --host=postgres --dbname=demo_db --username=user1
+```
---
+++ src/MartenApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using Marten;
+using Marten.PLv8;
+using Marten.PLv8.Patching;
+
+// Init store
+const string connectionString = "host=localhost;database=demo_db;password=Just4Now;username=user1";
+var store = DocumentStore.For(
+    cfg =>
+    {
+        cfg.Connection(connectionString);
+        cfg.UseJavascriptTransformsAndPatching();
+    }
+);
+
+// Remember items
+var items = new List<Item>();
+
+// Seed items
+await using (var session = store.LightweightSession())
+{
+    // Delete old items
+    session.DeleteWhere<Item>(_ => true);
+    await session.SaveChangesAsync();
+    Console.WriteLine("Deleted old data");
+    
+    // Create new items
+    for (var i = 0; i < 1000; i++)
+    {
+        var id = Guid.NewGuid();
+        var item = new Item {Id = id, Number = i};
+        items.Add(item);
+        session.Store(item);
+        Console.WriteLine($"Added item {id}");
+    }
+    await session.SaveChangesAsync();
+    Console.WriteLine("All items created");
+}
+
+// Check count
+await using (var session = store.QuerySession())
+{
+    var count = await session.Query<Item>().CountAsync();
+    Console.WriteLine($"Total items: {count}");
+}
+
+// Patch items concurrently
+var tasks = items.Select(item => PatchItemAsync(store, item)).ToList();
+await Task.WhenAll(tasks);
+
+// Say something
+Console.WriteLine("Done!");
+
+async Task PatchItemAsync(IDocumentStore store, Item item)
+{
+    await using var session = store.LightweightSession();
+    session.Patch<Item>(item.Id).Set(x => x.Number, item.Number * 10);
+    await session.SaveChangesAsync();
+}
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public int Number { get; set; }
+}
